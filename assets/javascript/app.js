4d4f3c5a428837e13e7628e20be503de4c70e8b8
joe <joeida@jpl.nasa.gov>
2016-08-15 10:21:47 -0700

    Added initial files to Repo

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDRGOPfsS3-aMRV-cyBAWcRSa2GJT9P4Kg",
+    authDomain: "train-time-76452.firebaseapp.com",
+    databaseURL: "https://train-time-76452.firebaseio.com",
+    storageBucket: "train-time-76452.appspot.com",
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$(document).ready(function() {
+
+});
